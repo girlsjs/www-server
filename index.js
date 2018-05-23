3cbfa1441afa4dfcbb68422d67e5d3c72f1c470c
Artur Siery <dotintegral@gmail.com>
2018-05-23 10:27:58 +0200

    Added synchronisation of themes dir

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const config = require('./config.json');
 const app = express();
 app.use(bodyParser.json());
 
-const projectDir = path.join(__dirname, 'www');
+const contentDir = path.join(__dirname, 'www');
+const themeDir = path.join(__dirname, 'www', 'pl', 'themes', 'www-theme');
+
 const baseDir = path.join(__dirname, 'www', 'pl');
 const buildDir = path.join(__dirname, 'www', 'build', 'pl');
 
-console.log(projectDir)
-console.log(baseDir)
-console.log(buildDir)
 
 const options = {
   config: '_prod-config.yml',
@@ -24,20 +23,27 @@ const options = {
 const hexo = new Hexo(baseDir, options);
 hexo.init();
 
-const git = simpleGit(projectDir);
+const pull = dir =>
+  new Promise((resolve) => {
+    const git = simpleGit(dir).silent(false);
+
+    console.log('--[Pulling]--', dir)
+
+    git.pull('origin', 'master', () =>
+      git.reset(['--hard', 'FETCH_HEAD'], resolve)
+    )
+  })
 
 
-const rebuild = () => git
-  .silent(false)
-  .pull('origin', 'master', () => {
-    git
-      .silent(false)
-      .reset(['--hard', 'FETCH_HEAD'], () => {
+const rebuild = () => {
+    pull(contentDir)
+      .then(() => pull(themeDir))
+      .then(() => {
         hexo
           .call('generate', {})
           .catch((e) => console.error(e));
       })
-  })
+  };
 
 
 app.post('/regenerate', (req, res) => {
@@ -48,6 +54,7 @@ app.post('/regenerate', (req, res) => {
     res.send('ok');
   }
 });
+
 
 app.use('/', express.static(buildDir));
 
